8e145363b328adac3552295c396b58458f30e6a1
Shaun Mbolompo <shaunmbolompo426@gmail.com>
2023-10-19 23:31:00 +0200

    Completed the nav bar UI

--- src/view/home.js
+++ src/view/home.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import styles from './style/home.module.css';
+import logo from './images/logo.svg';
+import { Link } from 'react-router-dom'; // Import Link from React Router
+
 
 function Home() {
   return (
     <div>
-      Home
+        <div className={styles.nav}>
+
+            {/* Logo container */}
+            <div className={styles.nav.inside}>
+                <img className={styles.logo} src={logo} alt=''/>
+            </div>
+
+            {/* Nav bar menu */}
+            <div className={styles.nabarMenu}>
+
+                <div className={styles.innerMenu}>
+                    <div className={styles.pageLink}>
+                        <Link to="/services" className={styles.link}>Services</Link>
+                    </div>
+                </div>
+                <div className={styles.innerMenu}>
+                    <div className={styles.pageLink}>
+                        <Link to="/services" className={styles.link}>Industries</Link>
+                    </div>
+                </div>
+                <div className={styles.innerMenu}>
+                    <div className={styles.pageLink}>
+                        <Link to="/services" className={styles.link}>Cases</Link>
+                    </div> 
+                </div>
+                <div className={styles.innerMenu}>
+                    <div className={styles.pageLink}>
+                        <Link to="/services" className={styles.link}>Contact</Link>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <button className={styles.navButton}>Let's Talk</button>
+            </div>
+        </div>
     </div>
   );
 }
