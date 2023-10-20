c0e5a1a76db9c5ae520d003f47ad45acbf14a3b6
Shaun Mbolompo <shaunmbolompo426@gmail.com>
2023-10-21 01:00:09 +0200

    Completed the app

--- src/view/home.js
+++ src/view/home.js
@@ -8,6 +8,21 @@ import blockchainIcon from './images/icon.svg';
 import olympian from './images/olympian.png';
 import savings from './images/savings.jpeg';
 import skhokho from './images/skhokho.jpeg';
+import visa from './images/visa.svg';
+import tyme from './images/tyme.svg';
+import distell from './images/distell.svg';
+import spotify from './images/spotify.svg';
+import microsoft from './images/microsoft.svg';
+import engen from './images/engen.svg';
+import nike from './images/nike.svg';
+import wesgrow from './images/wesgrow.svg';
+import multichoice from './images/multichoice.svg';
+import pnp from './images/pnp.svg';
+import liquid from './images/liquid.svg';
+import tfg from './images/tfg.svg';
+import sanlam from './images/sanlam.svg';
+import santam from './images/santam.svg';
+import bbc from './images/bbc.svg';
 
 
 import { Link } from 'react-router-dom'; // Import Link from React Router
@@ -83,7 +98,7 @@ function Home() {
 
                 <div className={styles.row}>
                     <div className={styles.fields}>
-                        <img className={styles.webIcon} src={webIcon} alt=''/>
+                        <img src={webIcon} alt=''/>
                         <div className={styles.rowTitle}>
                             Web development
                         </div>
@@ -92,7 +107,7 @@ function Home() {
                         </p>
                     </div>
                     <div className={styles.fields}>
-                        <img className={styles.uixIcon} src={uixIcon} alt=''/>
+                        <img src={uixIcon} alt=''/>
                         <div className={styles.rowTitle}>
                             User experience & design                       
                         </div>
@@ -101,7 +116,7 @@ function Home() {
                         </p>
                     </div>
                     <div className={styles.fields}>
-                        <img className={styles.appIcon} src={appIcon} alt=''/>
+                        <img src={appIcon} alt=''/>
                         <div className={styles.rowTitle}>
                             Mobile app development                   
                         </div>
@@ -110,7 +125,7 @@ function Home() {
                         </p>
                     </div>
                     <div className={styles.fields}>
-                        <img className={styles.blockchainIcon} src={blockchainIcon} alt=''/>
+                        <img src={blockchainIcon} alt=''/>
                         <div className={styles.rowTitle}>
                             Blockchain solutions                 
                         </div>
@@ -152,9 +167,140 @@ function Home() {
                 <div className={styles.brandsTitle}>
                     Trusted by leading brands
                 </div>
+
+                <div className={styles.row}>
+                    <div className={styles.column}>
+                        <img src={visa} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={tyme} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={distell} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={spotify} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={microsoft} alt=''/>
+                    </div>
+                </div>
+
+                <div className={styles.row}>
+                    <div className={styles.column}>
+                        <img src={engen} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={nike} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={wesgrow} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={multichoice} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={pnp} alt=''/>
+                    </div>
+                </div>
+
+                <div className={styles.row}>
+                    <div className={styles.column}>
+                        <img src={liquid} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={tfg} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={sanlam} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={santam} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img src={bbc} alt=''/>
+                    </div>
+                </div>
             </div>
         </div>
 
+        <div className={styles.footer}>
+        <div className={styles.firstInnerMiddle}>
+
+            <div className={styles.heading}>
+                <div className={styles.rectangleFooter}> 
+                </div>
+                <div className={styles.headingTextFooter}>
+                    Contact us
+                </div>
+            </div>
+
+            <div className={styles.row}>
+                <div className={styles.twoThird}>
+                Have a project in mind? <br></br>
+                Let's make it happen 
+                </div>
+                <div className={styles.third}>
+                    <span className={styles.space}> 22 Street Name, Suburb, 8000,</span>
+                    <br></br>
+                    <span className={styles.space}> Cape Town, South Africa </span>
+                    <br></br>
+                    +27 21 431 0001  <br></br>
+                    <span className={styles.email}>enquirie@website.co.za</span>
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles.twoThird}>
+                    <div className={styles.row}>
+                        <div className={styles.footerFields}>
+                            <p>
+                            <span className={styles.space}> Terms of service</span>
+                            <br></br>
+                            Privacy policy
+                            <br></br>
+                            Impressum
+                            </p>
+                        </div>
+                        <div className={styles.footerFields}>
+                            <p>
+                            Facebook
+                            <br></br>
+                            Instagram
+                            <br></br>
+                            Twitter
+                            </p>
+                        </div>
+                        <div className={styles.footerFields}>
+                            <p>
+                            Github
+                            <br></br>
+                            Linkedin
+                            <br></br>
+                            Teams
+                            </p>
+                        </div>
+                        <div className={styles.footerFields}>
+                            <p>
+                            Youtube
+                            <br></br>
+                            Behance
+                            <br></br>
+                            Dribbble
+                            <br></br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.lastThird}>
+                    <span className={styles.space}>Explore open jobs</span>
+                    <br></br>
+                    <br></br>
+
+                    <span className={styles.space}>©2000—2023 Company Name</span>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
   );
 }
