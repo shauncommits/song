2aa87a2e3438b603ba51ff7549c724b4b5b17d28
Shaun Mbolompo <shaunmbolompo426@gmail.com>
2023-10-20 23:45:10 +0200

    Added the middle part of the UI

--- src/view/home.js
+++ src/view/home.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import styles from './style/home.module.css';
 import logo from './images/logo.svg';
+import webIcon from './images/WebDevIcon.svg';
+import uixIcon from './images/UXIcon.svg';
+import appIcon from './images/AppDevIcon.svg';
+import blockchainIcon from './images/icon.svg';
+import olympian from './images/olympian.png';
+import savings from './images/savings.jpeg';
+import skhokho from './images/skhokho.jpeg';
+
+
 import { Link } from 'react-router-dom'; // Import Link from React Router
 
 
@@ -54,11 +63,98 @@ function Home() {
                         compaign
                      </div>
                      <div>
-                     <button className={styles.viewButton}>View project</button>
+                        <button className={styles.viewButton}>View project</button>
                      </div>
                 </div>
             </div>
         </div>
+        <div className={styles.middleBackground}>
+            <div className={styles.firstInnerMiddle}>
+                <div className={styles.heading}>
+                    <div className={styles.rectangle}> 
+                    </div>
+                    <div className={styles.headingText}>
+                       What we do
+                    </div>
+                </div>
+                <div className={styles.offer}>
+                    We offer a complete range of bespoke design and development services to help you turn your ideas into digital masterpieces
+                </div>
+
+                <div className={styles.row}>
+                    <div className={styles.fields}>
+                        <img className={styles.webIcon} src={webIcon} alt=''/>
+                        <div className={styles.rowTitle}>
+                            Web development
+                        </div>
+                        <p>
+                            We use cutting-edge web development technologies to help our clients fulfill their business goals through functional, reliable solutions.
+                        </p>
+                    </div>
+                    <div className={styles.fields}>
+                        <img className={styles.uixIcon} src={uixIcon} alt=''/>
+                        <div className={styles.rowTitle}>
+                            User experience & design                       
+                        </div>
+                        <p>
+                            Our complete web design services will bring your ideas to life and provide you with a sleek, high-performing product that elevates your business.
+                        </p>
+                    </div>
+                    <div className={styles.fields}>
+                        <img className={styles.appIcon} src={appIcon} alt=''/>
+                        <div className={styles.rowTitle}>
+                            Mobile app development                   
+                        </div>
+                        <p>
+                            Our extensive mobile development experience allows us to create custom native and cross-platform iOS and Android mobile solutions for our clients.
+                        </p>
+                    </div>
+                    <div className={styles.fields}>
+                        <img className={styles.blockchainIcon} src={blockchainIcon} alt=''/>
+                        <div className={styles.rowTitle}>
+                            Blockchain solutions                 
+                        </div>
+                        <p>
+                            We conduct market research to determine the optimal blockchain-based solutions to help you grow your company and achieve your business goals.
+                        </p>
+                    </div>
+                </div>
+
+
+                <div className={styles.caseHeading}>
+                    <div className={styles.rectangle}> 
+                    </div>
+                    <div className={styles.headingText}>
+                       Case Studies
+                    </div>
+                </div>
+
+                <div className={styles.row}>
+                    <div className={styles.column}>
+                        <img className={styles.caseImg} src={olympian} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img className={styles.caseImg} src={savings} alt=''/>
+                    </div>
+                    <div className={styles.column}>
+                        <img className={styles.caseImg} src={skhokho} alt=''/>
+                    </div>
+                </div>                
+
+                <div className={styles.heading}>
+                    <div className={styles.rectangle}> 
+                    </div>
+                    <div className={styles.headingText}>
+                       You'll be in good company
+                    </div>
+                </div>
+
+                <div className={styles.brandsTitle}>
+                    Trusted by leading brands
+                </div>
+            </div>
+        </div>
+
     </div>
   );
 }
